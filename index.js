b6d92d19002ec2578d27656a01c2ad1b425a6861
DeshmukhMandar3 <mandardeshmukh.mud@gmail.com>
2024-10-13 00:46:17 +0530

    added cors

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
-require('dotenv').config()
-const express=require("express");
-const mongoose=require("mongoose");
-const routes=require('./routes/routes')
-const mongoString=process.env.DATABASE_URL;
+require("dotenv").config();
+const cors = require("cors");
+const express = require("express");
+const mongoose = require("mongoose");
+const routes = require("./routes/routes");
+const mongoString = process.env.DATABASE_URL;
 
 mongoose.connect(mongoString);
-const database=mongoose.connection;
+const database = mongoose.connection;
 
-database.on('error',(error)=>{
-    console.log(error)
-})
+database.on("error", (error) => {
+  console.log(error);
+});
 
-database.once('connected',()=>{
-    console.log('Database connected')
-})
+database.once("connected", () => {
+  console.log("Database connected");
+});
 
-const app=express();
+const app = express();
 
 app.use(express.json());
+app.use(cors());
 
-app.use('/',routes)
+app.use("/", routes);
 
-app.listen(8000,()=>{
-    console.log("server connected to the port 8000")
-})
+app.listen(8000, () => {
+  console.log("server connected to the port 8000");
+});
